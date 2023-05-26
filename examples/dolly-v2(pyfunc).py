1679c801ad85039741f7c99320d84e6ae83af7a0
ahdbilal <55514721+ahdbilal@users.noreply.github.com>
2023-05-26 13:54:37 -0700

    adding prompting logic

--- examples/dolly-v2(pyfunc).py
+++ examples/dolly-v2(pyfunc).py
@@ -14,30 +14,60 @@ snapshot_location = snapshot_download(repo_id="databricks/dolly-v2-3b")
 # COMMAND ----------
 
 class Dolly(mlflow.pyfunc.PythonModel):
-  def load_context(self, context):
-    # Initialize tokenizer and language model
-    self.tokenizer = transformers.AutoTokenizer.from_pretrained(
-      context.artifacts['repository'], padding_side="left")
-    self.model = transformers.AutoModelForCausalLM.from_pretrained(
-      context.artifacts['repository'], torch_dtype=torch.bfloat16, 
-      low_cpu_mem_usage=True, device_map="auto",
-      pad_token_id=self.tokenizer.eos_token_id).to('cuda')
-    self.model.eval()
+    def load_context(self, context):
+        """
+        This method initializes the tokenizer and language model
+        using the specified model repository.
+        """
+        # Initialize tokenizer and language model
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            context.artifacts['repository'], padding_side="left")
+        self.model = transformers.AutoModelForCausalLM.from_pretrained(
+            context.artifacts['repository'], torch_dtype=torch.bfloat16,
+            low_cpu_mem_usage=True, device_map="auto",
+            pad_token_id=self.tokenizer.eos_token_id).to('cuda')
+        self.model.eval()
 
-  def predict(self, context, model_input):
-    message = model_input["message"][0]
-    temperature = model_input.get("temperature", [1.0])[0]
-    max_tokens = model_input.get("max_tokens", [100])[0]
+    def _build_prompt(self, instruction):
+        """
+        This method generates the prompt for the model.
+        """
+        INSTRUCTION_KEY = "### Instruction:"
+        RESPONSE_KEY = "### Response:"
+        INTRO_BLURB = (
+            "Below is an instruction that describes a task. "
+            "Write a response that appropriately completes the request."
+        )
+
+        return f"""{INTRO_BLURB}
+        {INSTRUCTION_KEY}
+        {instruction}
+        {RESPONSE_KEY}
+        """
+
+    def predict(self, context, model_input):
+        """
+        This method generates prediction for the given input.
+        """
+        message = model_input["message"][0]
+        temperature = model_input.get("temperature", [1.0])[0]
+        max_tokens = model_input.get("max_tokens", [100])[0]
+
+        # Build the prompt
+        prompt = self._build_prompt(message)
+
+        # Encode the input and generate prediction
+        encoded_input = self.tokenizer.encode(prompt, return_tensors='pt').to('cuda')
+        output = self.model.generate(encoded_input, do_sample=True, temperature=temperature, max_length=max_tokens)
     
-    # Encode input and generate prediction
-    encoded_input = self.tokenizer.encode(message, return_tensors='pt').to('cuda')
-    
-    # Decode prediction to text
-    with torch.no_grad():
-      output = self.model.generate(encoded_input, do_sample=True, temperature=temperature, max_length=max_tokens)
-      generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
-    
-    return generated_text
+        # Decode the prediction to text
+        generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
+
+        # Removing the prompt from the generated text
+        prompt_length = len(self.tokenizer.encode(prompt, return_tensors='pt')[0])
+        generated_response = self.tokenizer.decode(output[0][prompt_length:], skip_special_tokens=True)
+
+        return generated_response
 
 # COMMAND ----------
 
