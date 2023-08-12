875f86539583e4bef2d7359e0b11ddbe56ae114d
ahdbilal <ahdbilal>
2023-08-12 01:33:08 +0000

    update llama

--- examples/llama-2-7b-chat-hf.py
+++ examples/llama-2-7b-chat-hf.py
@@ -1,4 +1,12 @@
 # Databricks notebook source
+# Databricks notebook source
+!pip install --upgrade transformers
+!pip install --upgrade accelerate
+!pip install --upgrade mlflow
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
 import pandas as pd
 import numpy as np
 import transformers
@@ -8,10 +16,25 @@ import torch
 
 # COMMAND ----------
 
-model = "meta-llama/Llama-2-7b-chat-hf"
-repository = snapshot_download(repo_id=model, ignore_patterns="*.safetensors")
+import huggingface_hub
+#skip this if you are already logged in to hugging face
+huggingface_hub.login()
 
 # COMMAND ----------
+
+model = "meta-llama/Llama-2-7b-chat-hf"
+repository = huggingface_hub.snapshot_download(repo_id=model, ignore_patterns="*.safetensors")
+
+# COMMAND ----------
+
+# Define prompt template to get the expected features and performance for the chat versions. See our reference code in github for details: https://github.com/facebookresearch/llama/blob/main/llama/generation.py#L212
+
+DEFAULT_SYSTEM_PROMPT = """\
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+
+# Define PythonModel to log with mlflow.pyfunc.log_model
 
 class Llama2(mlflow.pyfunc.PythonModel):
     def load_context(self, context):
@@ -20,27 +43,33 @@ class Llama2(mlflow.pyfunc.PythonModel):
         using the specified model repository.
         """
         # Initialize tokenizer and language model
-        self.tokenizer = LlamaTokenizerFast.from_pretrained(
-          context.artifacts['repository'])
-
-        self.model = LlamaForCausalLM.from_pretrained(
-          context.artifacts['repository'], 
-          torch_dtype=torch.float16, 
-          low_cpu_mem_usage=True, 
-          device_map='auto')
-        
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            context.artifacts['repository'], padding_side="left")
+        self.model = transformers.AutoModelForCausalLM.from_pretrained(
+            context.artifacts['repository'], 
+            torch_dtype=torch.bfloat16,
+            low_cpu_mem_usage=True, 
+            trust_remote_code=True,
+            device_map="auto",
+            pad_token_id=self.tokenizer.eos_token_id)
         self.model.eval()
 
-    def predict(self, context, model_input):
+    def _build_prompt(self, instruction):
         """
-        This method generates prediction for the given input.
+        This method generates the prompt for the model.
         """
-        prompt = model_input["prompt"][0]
-        max_tokens = model_input.get("max_tokens", [100])[0]
+        return f"""<s>[INST]<<SYS>>\n{DEFAULT_SYSTEM_PROMPT}\n<</SYS>>\n\n\n{instruction}[/INST]\n"""
+
+    def _generate_response(self, prompt, temperature, max_new_tokens):
+        """
+        This method generates prediction for a single input.
+        """
+        # Build the prompt
+        prompt = self._build_prompt(prompt)
 
         # Encode the input and generate prediction
         encoded_input = self.tokenizer.encode(prompt, return_tensors='pt').to('cuda')
-        output = self.model.generate(encoded_input, do_sample=True, max_new_tokens=max_tokens)
+        output = self.model.generate(encoded_input, do_sample=True, temperature=temperature, max_new_tokens=max_new_tokens)
     
         # Decode the prediction to text
         generated_text = self.tokenizer.decode(output[0], skip_special_tokens=True)
@@ -50,6 +79,22 @@ class Llama2(mlflow.pyfunc.PythonModel):
         generated_response = self.tokenizer.decode(output[0][prompt_length:], skip_special_tokens=True)
 
         return generated_response
+      
+    def predict(self, context, model_input):
+        """
+        This method generates prediction for the given input.
+        """
+
+        outputs = []
+
+        for i in range(len(model_input)):
+          prompt = model_input["prompt"][i]
+          temperature = model_input.get("temperature", [1.0])[i]
+          max_new_tokens = model_input.get("max_new_tokens", [100])[i]
+
+          outputs.append(self._generate_response(prompt, temperature, max_new_tokens))
+      
+        return outputs
 
 # COMMAND ----------
 
@@ -60,5 +105,9 @@ with mlflow.start_run() as run:
         artifacts={'repository' : repository},
         pip_requirements=["torch", "transformers", "accelerate"],
         input_example=pd.DataFrame({"prompt":["what is ML?"],"max_tokens": [80]}),
-        registered_model_name='llama2-7b'
+        registered_model_name='bilal-llama2-7b'
     )
+
+# COMMAND ----------
+
+
